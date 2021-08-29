cf36e3b22db6191f51afd9cc432d2a620ed0325d
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2021-08-29 16:45:13 +0000

    stage six completion

--- Hangman (1)/task/hangman/hangman.py
+++ Hangman (1)/task/hangman/hangman.py
@@ -4,8 +4,7 @@ words = ["python", "java", "kotlin", "javascript"]
 number_of_tries = 8
 dash_line = []
 
-print("H A N G M A N")
-print()
+print("H A N G M A N\n")
 random_selected_word = words[random.randint(0, len(words) - 1)]
 random_selected_word = list(random_selected_word)
 len_rand_select_word = len(random_selected_word)
@@ -13,24 +12,32 @@ len_rand_select_word = len(random_selected_word)
 for letters in range(0, len_rand_select_word):
     dash_line += "-"
 
-# print()
 while 0 < number_of_tries < 9:
-    for x in dash_line:
-        print(x, end="")
-    print()
-
-    guess_letter = input("Input a letter: ")
+    print(''.join(str(i) for i in dash_line))
+    guess_letter = input("Input a letter:")
 
     if guess_letter in random_selected_word:
-        print()
-        for x in range(len_rand_select_word):
-            if guess_letter == random_selected_word[x]:
-                dash_line[x] = guess_letter
-        number_of_tries -= 1
+        if guess_letter in dash_line:
+            number_of_tries -= 1
+            print("No improvements")
+            if number_of_tries == 0:
+                print("You lost!")
+            print()
+
+        else:
+            print()
+            for x in range(len_rand_select_word):
+                if guess_letter == random_selected_word[x]:
+                    dash_line[x] = guess_letter
+                if dash_line == random_selected_word and number_of_tries > 0:
+                    number_of_tries = 0
+                    print(''.join(str(i) for i in dash_line))
+                    print("You guessed the word!")
+                    print("You survived!")
+
     else:
         number_of_tries -= 1
         print("That letter doesn't appear in the word")
+        if number_of_tries == 0:
+            print("You lost!")
         print()
-
-print("Thanks for playing!")
-print("We'll see how well you did in the next stage")
